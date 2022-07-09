5c6ae35a3fdd9ca9c385685d7f89b073a4c070dc
Jordi Burgos <jordiburgos@gmail.com>
2022-07-10 00:17:02 +0200

    Create main library files

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Start() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome GAA!")
+	})
+
+	// fs := http.FileServer(http.Dir("static/"))
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	fmt.Println("Server started on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+
+}
