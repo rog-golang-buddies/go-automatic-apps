55259dcda954014ea341b9643cab3beb70076510
Jordi Burgos <jordiburgos@gmail.com>
2022-07-18 17:51:56 +0200

    Create server and templates

--- server/main.go
+++ server/main.go
@@ -1,27 +1,34 @@
 package server
 
 import (
+	"embed"
 	"fmt"
+	"html/template"
 	"net/http"
-	"strings"
 
+	"github.com/gin-gonic/gin"
 	"github.com/rog-golang-buddies/go-automatic-apps/database"
 )
 
+//go:embed templates/*
+var content embed.FS
+
 func Start() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		tables := strings.Join(database.GetTables(), "<br/>")
+	fmt.Println("Starting server...")
 
-		fmt.Fprintf(w, "<html><body>Welcome to GAA! <br><br>"+tables+"</body></html>")
+	templates := template.Must(template.ParseFS(content, "templates/**"))
+	router := gin.Default()
+	router.SetHTMLTemplate(templates)
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+	})
+	router.GET("/config", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "config.tmpl", gin.H{
+			"tables": database.GetTables(),
+		})
 	})
 
-	// fs := http.FileServer(http.Dir("static/"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
-
 	fmt.Println("Server started on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
+	router.Run()
 }
