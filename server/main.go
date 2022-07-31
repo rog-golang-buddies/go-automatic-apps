d1ef4fd7284ad31a5cb9db5074855474255390c4
Parham Moieni <pmoieni06@gmail.com>
2022-07-31 11:31:20 +0430

    implemented Cors and graceful shutdown

--- server/main.go
+++ server/main.go
@@ -1,15 +1,21 @@
 package server
 
 import (
+	"context"
 	"embed"
 	"fmt"
 	"io/fs"
 	"log"
 	"net"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"entgo.io/ent/dialect/sql/schema"
 	"github.com/rog-golang-buddies/go-automatic-apps/rpc"
+	"github.com/rs/cors"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 )
 
@@ -36,6 +42,30 @@ func Start(config ServerConfig) {
 		config.GRPCPort = "8081"
 	}
 
+	// NotifyContext for server graceful shutdown
+	serverCtx, serverStop := signal.NotifyContext(context.Background(), syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	defer serverStop()
+
+	// create a new serve mux to attach handlers to it and enable Cors policy
+	mux := http.NewServeMux()
+
+	c := cors.Options{
+		AllowedOrigins: []string{"*"},
+	}
+	handler := cors.New(c).Handler(mux)
+
+	httpServer := http.Server{
+		Addr:         config.Host + ":" + config.HttpPort,
+		Handler:      handler,
+		ErrorLog:     log.Default(),     // set the logger for the server
+		ReadTimeout:  10 * time.Second,  // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+		BaseContext: func(_ net.Listener) context.Context {
+			return serverCtx
+		},
+	}
+
 	fmt.Println("Starting server...")
 
 	webRoot, err := fs.Sub(webDistEmbed, "web/dist")
@@ -43,29 +73,35 @@ func Start(config ServerConfig) {
 		log.Fatal(err)
 		panic(err)
 	}
-	http.Handle("/", http.FileServer(http.FS(webRoot)))
+	mux.Handle("/", http.FileServer(http.FS(webRoot)))
+
+	g, gCtx := errgroup.WithContext(serverCtx)
+	g.Go(func() error {
+		// Run the server
+		return httpServer.ListenAndServe()
+	})
 
 	grpcServer := grpc.NewServer()
 	databaseService := rpc.NewDatabaseService()
 	rpc.RegisterDatabaseServiceServer(grpcServer, databaseService)
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", config.GRPCPort))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	go func() {
-		var grpcServerUrl = config.Host + ":" + config.GRPCPort
-		log.Println("gRPC server started at " + grpcServerUrl)
-		err := grpcServer.Serve(lis)
+	g.Go(func() error {
+		lis, err := net.Listen("tcp", fmt.Sprintf(":%s", config.GRPCPort))
 		if err != nil {
-			log.Fatal("RpcServe: ", err)
+			log.Panicf("grpc listen: %v", err.Error())
+			return err
 		}
-	}()
 
-	var httpServerUrl = config.Host + ":" + config.HttpPort
-	log.Println("http server started at " + httpServerUrl)
-	err = http.ListenAndServe(httpServerUrl, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+		err = grpcServer.Serve(lis)
+		if err != nil {
+			log.Panicf("grpc serve: %v", err.Error())
+			return err
+		}
+
+		return nil
+	})
+	g.Go(func() error {
+		<-gCtx.Done()
+		grpcServer.GracefulStop()
+		return httpServer.Shutdown(context.Background())
+	})
 }
