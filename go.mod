a0eedb73a19c3d59a5d0af1c0f23800ae0ab5484
segfault99 <segfault99@protonmail.com>
2022-08-02 12:18:49 +0300

    moved to rest

--- go.mod
+++ go.mod
@@ -4,10 +4,9 @@ go 1.18
 
 require (
 	entgo.io/ent v0.11.1
+	github.com/go-chi/chi/v5 v5.0.7
 	github.com/rs/cors v1.8.2
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
-	google.golang.org/grpc v1.48.0
-	google.golang.org/protobuf v1.28.1
 )
 
 require github.com/sergi/go-diff v1.2.0 // indirect
@@ -17,7 +16,6 @@ require (
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
@@ -27,9 +25,7 @@ require (
 	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/zclconf/go-cty v1.10.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
 	golang.org/x/sys v0.0.0-20220730100132-1609e554cd39 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.12 // indirect
-	google.golang.org/genproto v0.0.0-20220728213248-dd149ef739b9 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -3,17 +3,13 @@ package server
 import (
 	"context"
 	"embed"
-	"fmt"
-	"io/fs"
 	"log"
-	"net"
 	"net/http"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"entgo.io/ent/dialect/sql/schema"
-	"github.com/rs/cors"
+	"github.com/rog-golang-buddies/go-automatic-apps/server/httpd"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -27,8 +23,7 @@ type ServerConfig struct {
 var webDistEmbed embed.FS
 
 func Start(config ServerConfig) {
-
-	// Set defauls
+	// Set defaults
 	if config.Host == "" {
 		config.Host = "localhost"
 	}
@@ -39,43 +34,18 @@ func Start(config ServerConfig) {
 	// NotifyContext for server graceful shutdown
 	serverCtx, serverStop := signal.NotifyContext(context.Background(), syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	defer serverStop()
-
-	// create a new serve mux to attach handlers to it and enable Cors policy
-	mux := http.NewServeMux()
-
-	c := cors.Options{
-		AllowedOrigins: []string{"*"},
-	}
-	handler := cors.New(c).Handler(mux)
-
-	httpServer := http.Server{
-		Addr:         config.Host + ":" + config.HttpPort,
-		Handler:      handler,
-		ErrorLog:     log.Default(),     // set the logger for the server
-		ReadTimeout:  10 * time.Second,  // max time to read request from the client
-		WriteTimeout: 10 * time.Second,  // max time to write response to the client
-		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
-		BaseContext: func(_ net.Listener) context.Context {
-			return serverCtx
-		},
-	}
-
-	fmt.Println("Starting server...")
-
-	webRoot, err := fs.Sub(webDistEmbed, "web/dist")
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
-	mux.Handle("/", http.FileServer(http.FS(webRoot)))
-
+    
+    controller := httpd.NewController(webDistEmbed)
+	
 	g, gCtx := errgroup.WithContext(serverCtx)
 	g.Go(func() error {
-		// Run the server
-		return httpServer.ListenAndServe()
-	})
-	g.Go(func() error {
 		<-gCtx.Done()
-		return httpServer.Shutdown(context.Background())
+        log.Println("Shutting down the server")
+		return controller.Shutdown(context.Background())
 	})
+
+    err := controller.Start(serverCtx, config.Host, config.HttpPort)
+    if err != http.ErrServerClosed {
+        log.Println("Shutting down the server has failed")
+    }
 }
